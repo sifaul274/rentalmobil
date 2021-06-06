c619ea020c538a16ae25d7c6ddd7326f4793727a
sifaul274 <82395907+sifaul274@users.noreply.github.com>
2021-06-06 19:16:58 +0700

    Update ListMobil.java

--- ListMobil.java
+++ ListMobil.java
@@ -1,6 +1,12 @@
 package rentalmobil;
 public class ListMobil extends Mobil{
-    protected int hargaSewa;
+    protected int jangkaWaktu, hargaSewa;
+    void setJangkaWaktu(int jangkaWaktu){
+        this.jangkaWaktu = jangkaWaktu;
+    }
+    int getJangkaWaktu(){
+        return jangkaWaktu;
+    }
     void setHargaSewa(int hargaSewa){
         this.hargaSewa = hargaSewa;
     }
@@ -15,8 +21,15 @@ public class ListMobil extends Mobil{
     String getNamaMobil() {
         return namaMobil;
     }
-    ListMobil(String namaMobil, int hargaSewa){
+    @Override
+    void setWarna(String warna) {
+        this.warna = warna;
+    }
+    @Override
+    String getWarna() {
+        return warna;
+    }
+    ListMobil(String namaMobil){
         this.namaMobil = namaMobil;
-        this.hargaSewa = hargaSewa;
     }
 }
