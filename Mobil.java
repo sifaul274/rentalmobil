ed6397df24f6d6de1571eebfa8e3695edc37d583
sifaul274 <82395907+sifaul274@users.noreply.github.com>
2021-06-06 19:16:18 +0700

    Update Mobil.java

--- Mobil.java
+++ Mobil.java
@@ -1,6 +1,8 @@
 package rentalmobil;
 abstract class Mobil {
-    protected String namaMobil;
+    protected String namaMobil, warna;
     abstract void setNamaMobil(String namaMobil);
     abstract String getNamaMobil();
+    abstract void setWarna(String warna);
+    abstract String getWarna();
 }
