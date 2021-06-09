cd85c72ec7faa7a46f40583ebd97374740e687c1
sifaul274 <82395907+sifaul274@users.noreply.github.com>
2021-06-09 09:37:18 +0700

    Update RentalMobil.java

--- RentalMobil.java
+++ RentalMobil.java
@@ -1,15 +1,17 @@
 package rentalmobil;
 import java.util.Scanner;
 public class RentalMobil {
+    private static String pass_adm = "kelompok9";
     public static void main(String[] args) {
         Scanner ketik = new Scanner(System.in);
+        ToolsMenu tools = new ToolsMenu();
         
         String[]nMobil = {"Daihatsu Ayla","Honda Brio   ","Suzuki Ertiga","Nissan Livina","Toyota Avanza","Daihatsu Xenia","Toyota Innova","Toyota HiAce","Isuzu Elf    ","Jeep             "};
         String[]wMobil = {"Merah","Kuning","Silver","Oranye","Bronze","Biru","Putih","Putih","Putih","Hitam"};
         int[]jWaktu = {24, 24, 24, 24, 24, 24, 24, 24, 24, 24};
         int[]hSewa = {325000, 325000, 375000, 375000, 375000, 375000, 475000, 700000, 500000, 645000};
         ListMobil[]listM = new ListMobil[nMobil.length];
-        Tools.pMobil(listM, nMobil, wMobil, jWaktu, hSewa);
+        ToolsMenu.pMobil(listM, nMobil, wMobil, jWaktu, hSewa);
         int no_mobil;
         
         String cNama, cAlamat, cNoKTP, cNoTelp, cEmail;
@@ -18,7 +20,7 @@ public class RentalMobil {
         String back;
         String p_menu;
         while(app==1){
-            Tools.menu();
+            ToolsMenu.menu();
             p_menu = ketik.nextLine();
             if (p_menu.equals("1")) {
                 System.out.println("");
@@ -28,7 +30,7 @@ public class RentalMobil {
                     back = ketik.nextLine();
                     System.out.println("");
                 } else {
-                    Tools.listMobil(listM);
+                    ToolsMenu.listMobil(listM);
                     System.out.print("Tekan Enter untuk keluar...");
                     back = ketik.nextLine();
                     System.out.println("");
@@ -41,16 +43,16 @@ public class RentalMobil {
                     back = ketik.nextLine();
                     System.out.println("");
                 } else {
-                    Tools.listMobil(listM);
-                    no_mobil = Tools.inputInt("Nomor Mobil") - 1;
-                    cNama = Tools.inputString("Nama");
-                    cAlamat = Tools.inputString("Alamat");
-                    cNoKTP = Tools.inputString("No. KTP");
-                    cNoTelp = Tools.inputString("No. Telp");
-                    cEmail = Tools.inputString("Email");
+                    ToolsMenu.listMobil(listM);
+                    no_mobil = tools.inputInt("Nomor Mobil") - 1;
+                    cNama = tools.inputString("Nama");
+                    cAlamat = tools.inputString("Alamat");
+                    cNoKTP = tools.inputString("No. KTP");
+                    cNoTelp = tools.inputString("No. Telp");
+                    cEmail = tools.inputString("Email");
                     System.out.println("");
-                    Tools.cetak(cNama, cAlamat, cNoKTP, cNoTelp, cEmail, listM[no_mobil]);
-                    listM = Tools.removeArray(listM, no_mobil);
+                    ToolsMenu.cetak(cNama, cAlamat, cNoKTP, cNoTelp, cEmail, listM[no_mobil]);
+                    listM = tools.hapusArray(listM, no_mobil);
                     System.out.print("Tekan Enter untuk keluar...");
                     back = ketik.nextLine();
                     System.out.println("");
@@ -59,14 +61,22 @@ public class RentalMobil {
                 app = 0;
                 System.out.println("");
             } else if (p_menu.equals("4dm1n")) {
-                System.out.println("\n!!!!!!!!!!!!!!!!!!!!FORBIDDEN!!!!!!!!!!!!!!!!!!!!");
-                String namaMB, warnaMB;
-                int tarifMB;
-                namaMB = Tools.inputString("Nama Mobil Baru");
-                warnaMB = Tools.inputString("Warna Mobil");
-                tarifMB = Tools.inputInt("Tarif/24jam");
-                listM = Tools.tambahArray(listM, namaMB, warnaMB, tarifMB);
-                System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+                System.out.println("");
+                String pass = tools.inputString("Password");
+                if (pass.equals(pass_adm)) {
+                    System.out.println("\n!!!!!!!!!!!!!!!!!!!!FORBIDDEN!!!!!!!!!!!!!!!!!!!!");
+                    System.out.println("!!!!!!!!!!!!!***Tambah List Mobil***!!!!!!!!!!!!!");
+                    String namaMB, warnaMB;
+                    int tarifMB;
+                    namaMB = tools.inputString("Nama Mobil Baru");
+                    warnaMB = tools.inputString("Warna Mobil");
+                    tarifMB = tools.inputInt("Tarif/24jam");
+                    listM = tools.tambahArray(listM, namaMB, warnaMB, tarifMB);
+                    System.out.println("Mobil Berhasil Ditambahkan");
+                    System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+                } else {
+                    System.out.println("Password salah\n");
+                }
             } else {
                 System.out.println("");
             }
